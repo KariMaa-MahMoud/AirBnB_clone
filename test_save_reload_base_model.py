af9eef33d8d682bef8e0e7353c43e1b2b8ad8212
KariMaa-MahMoud <karima.mahmoud10@gmail.com>
2024-02-12 23:05:09 +0200

    f

--- test_save_reload_base_model.py
+++ test_save_reload_base_model.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-from models.engine.file_storage import FileStorage
+from models import storage
+from models.base_model import BaseModel
 
-
-storage = FileStorage()
 all_objs = storage.all()
 print("-- Reloaded objects --")
 for obj_id in all_objs.keys():
     obj = all_objs[obj_id]
     print(obj)
 
-from models.base_model import BaseModel
 
 print("-- Create a new object --")
 my_model = BaseModel()
