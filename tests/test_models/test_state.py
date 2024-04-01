31f1cc6d467230e72c1e1d8afc9673a678f8768c
KariMaa-MahMoud <128936629+KariMaa-MahMoud@users.noreply.github.com>
2024-04-01 22:49:17 +0200

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -7,9 +7,7 @@ import time
 from models.state import State
 import re
 import json
-from models.engine.file_storage import FileStorage
 import os
-from models import storage
 from models.base_model import BaseModel
 
 
@@ -25,7 +23,8 @@ class TestState(unittest.TestCase):
         """Tears down test methods."""
         self.resetStorage()
         pass
-
+        
+from models.engine.file_storage import FileStorage
     def resetStorage(self):
         """Resets FileStorage data."""
         FileStorage._FileStorage__objects = {}
@@ -39,7 +38,8 @@ class TestState(unittest.TestCase):
         self.assertEqual(str(type(b)), "<class 'models.state.State'>")
         self.assertIsInstance(b, State)
         self.assertTrue(issubclass(type(b), BaseModel))
-
+        
+from models import storage
     def test_8_attributes(self):
         """Tests the attributes of State class."""
         attributes = storage.attributes()["State"]
