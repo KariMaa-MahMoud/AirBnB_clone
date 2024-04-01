7367ed4fbba07e6da376fc3607b94001ff93de24
KariMaa-MahMoud <128936629+KariMaa-MahMoud@users.noreply.github.com>
2024-04-01 22:48:11 +0200

    Update test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -7,9 +7,7 @@ import time
 from models.place import Place
 import re
 import json
-from models.engine.file_storage import FileStorage
 import os
-from models import storage
 from models.base_model import BaseModel
 
 
@@ -25,7 +23,8 @@ class TestPlace(unittest.TestCase):
         """Tears down test methods."""
         self.resetStorage()
         pass
-
+        
+from models.engine.file_storage import FileStorage
     def resetStorage(self):
         """Resets FileStorage data."""
         FileStorage._FileStorage__objects = {}
@@ -39,7 +38,8 @@ class TestPlace(unittest.TestCase):
         self.assertEqual(str(type(b)), "<class 'models.place.Place'>")
         self.assertIsInstance(b, Place)
         self.assertTrue(issubclass(type(b), BaseModel))
-
+        
+from models import storage
     def test_8_attributes(self):
         """Tests the attributes of Place class."""
         attributes = storage.attributes()["Place"]
